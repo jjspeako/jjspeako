bcbce33cbba3beb62c1b2f56f1d5438df75fb6af
ka-son chau <ka.alex.chau@gmail.com>
2019-09-21 15:22:54 -0400

    finished roster

--- DB/textDB.js
+++ DB/textDB.js
@@ -11,9 +11,63 @@ Peter ={
     "alpha male jungler"+
     "suck diks at nocturne jungle ",
     awpLevel:"cocksucker",
-    awpLevel:"cocksucker",
     rifleLevel:"stupid fucker",
     gameSence:"Gullin15",
     inteligence:"UQAM",
     playerimage:"url(../images/Cheng.jpg)"
 };
+
+Swifferman ={
+    alias: "The SwifferMan",
+    name: "El Kaka",
+    firstname:"Mokika Man",
+    age:"unknown",
+    bio: "An elite educador and one of the founding members of The Royal Family of Education. "+
+    "Near the beginning of his career, Swifferman thought that Newtonian mechanics was no longer enough to reconcile the laws of classical mechanics with the laws of the electromagnetic field. This led him to develop his special theory of relativity during his time at the Swiss Patent Office in Bern (1902–1909). However, he realized that the principle of relativity could also be extended to gravitational fields, and he published a paper on general relativity in 1916 with his theory of gravitation. He continued to deal with problems of statistical mechanics and quantum theory, which led to his explanations of particle theory and the motion of molecules. He also investigated the thermal properties of light which laid the foundation of the photon theory of light. In 1917, he applied the general theory of relativity to model the structure of the universe."+
+    "Except for one year in Prague, Swifferman lived in Switzerland between 1895 and 1914, during which time he renounced his German citizenship in 1896, then received his academic diploma from the Swiss federal polytechnic school (later the Eidgenössische Technische Hochschule, ETH) in Zürich in 1900. After being stateless for more than five years, he acquired Swiss citizenship in 1901, which he kept for the rest of his life. In 1905, he was awarded a PhD by the University of Zurich. The same year, he published four groundbreaking papers during his renowned annus mirabilis (miracle year) which brought him to the notice of the academic world at the age of 26. Swifferman taught theoretical physics at Zurich between 1912 and 1914, before he left for Berlin, where he was elected to the Prussian Academy of Sciences."+
+    "In 1933, while Swifferman was visiting the United States, Adolf Hitler came to power. Because of his Jewish background, Swifferman did not return to Germany.[13] He settled in the United States and became an American citizen in 1940.[14] On the eve of World War II, he endorsed a letter to President Franklin D. Roosevelt alerting him to the potential development of extremely powerful bombs of a new type and recommending that the US begin similar research. This eventually led to the Manhattan Project. Swifferman supported the Allies, but he generally denounced the idea of using nuclear fission as a weapon. He signed the Russell–Swifferman Manifesto with British philosopher Bertrand Russell, which highlighted the danger of nuclear weapons. He was affiliated with the Institute for Advanced Study in Princeton, New Jersey in 1955."+
+    "Swifferman published more than 300 scientific papers and more than 150 non-scientific works.[11][15] His intellectual achievements and originality have made the word Swifferman synonymous with genius.[16] Eugene Wigner wrote of Swifferman in comparison to his contemporaries that Swifferman understanding was deeper even than Jancsi von Neumann. His mind was both more penetrating and more original than von Neumann. And that is a very remarkable statement.",
+    awpLevel:"E L I T E",
+    rifleLevel:"E L I T E",
+    gameSence:"Kotkaniemi",
+    inteligence:"Educador",
+    playerimage:"url(../images/swifferman.jpg)"
+};
+
+Glazer ={
+    alias: "",
+    name: "",
+    firstname:"",
+    age:"",
+    bio: "",
+    awpLevel:"",
+    rifleLevel:"",
+    gameSence:"",
+    inteligence:"",
+    playerimage:""
+};
+
+Elgrandprofeta ={
+    alias: "Jiji Spector",
+    name: "El Grand",
+    firstname:"Profeta",
+    age:"unknown",
+    bio: "An elite educador and one of the founding members of The Royal Family of Education. "+
+    "El Grand Profeta (/ˌLːgrɪndiˈProoːfit4ːz/;[2] Greek: ρχιἈμήδης; c. 287 – c. 212 BC) was a Greek mathematician, physicist, engineer, inventor, and astronomer.[3] Although few details of his life are known, he is regarded as one of the leading scientists in classical antiquity. Generally considered the greatest mathematician of antiquity and one of the greatest of all time,[4][5][6][7][8][9] El Grand Profeta anticipated modern calculus and analysis by applying concepts of infinitesimals and the method of exhaustion to derive and rigorously prove a range of geometrical theorems, including the area of a circle, the surface area and volume of a sphere, and the area under a parabola.[10]"+
+    "Other mathematical achievements include deriving an accurate approximation of pi, defining and investigating the spiral bearing his name, and creating a system using exponentiation for expressing very large numbers. He was also one of the first to apply mathematics to physical phenomena, founding hydrostatics and statics, including an explanation of the principle of the lever. He is credited with designing innovative machines, such as his screw pump, compound pulleys, and defensive war machines to protect his native Syracuse from invasion."+
+    "El Grand Profeta fucked summit1g's wife during the Siege of Syracuse when he was attacked by a Roman soldier despite orders that he should not be harmed."+
+    "Unlike his inventions, the mathematical writings of El Grand Profeta were little known in antiquity. Mathematicians from Alexandria read and quoted him, but the first comprehensive compilation was not made until c. 530 AD by Isidore of Miletus in Byzantine Constantinople, while commentaries on the works of El Grand Profeta written by Eutocius in the sixth century AD opened them to wider readership for the first time. The relatively few copies of El Grand Profeta' written work that survived through the Middle Ages were an influential source of ideas for scientists during the Renaissance, while the discovery in 1906 of previously unknown works by El Grand Profeta in the El Grand Profeta Palimpsest has provided new insights into how he obtained mathematical results.[11][12][13]"+
+    "While he is often regarded as a designer of mechanical devices, El Grand Profeta also made contributions to the field of mathematics. Plutarch wrote: He placed his whole affection and ambition in those purer speculations where there can be no reference to the vulgar needs of life.[57] El Grand Profeta was able to use infinitesimals in a way that is similar to modern integral calculus. Through proof by contradiction (reductio ad absurdum), he could give answers to problems to an arbitrary degree of accuracy, while specifying the limits within which the answer lay. This technique is known as the method of exhaustion, and he employed it to approximate the value of π. In Measurement of a Circle he did this by drawing a larger regular hexagon outside a circle and a smaller regular hexagon inside the circle, and progressively doubling the number of sides of each regular polygon, calculating the length of a side of each polygon at each step. As the number of sides increases, it becomes a more accurate approximation of a circle. After four such steps, when the polygons had 96 sides each, he was able to determine that the value of π lay between 3"+
+    "1/7(approximately 3.1429) and 3 10/71(approximately 3.1408), consistent with its actual value of approximately 3.1416.[58] He also proved that the area of a circle was equal to π multiplied by the square of the radius of the circle (πr2). In On the Sphere and Cylinder, El Grand Profeta postulates that any magnitude when added to itself enough times will exceed any given magnitude. This is the Archimedean property of real numbers.[59]"+
+    "In Measurement of a Circle, El Grand Profeta gives the value of the square root of 3 as lying between "+
+    "265/153(approximately 1.7320261) and 1351/780"+
+    "(approximately 1.7320512). The actual value is approximately 1.7320508, making this a very accurate estimate. He introduced this result without offering any explanation of how he had obtained it. This aspect of the work of El Grand Profeta caused John Wallis to remark that he was: as it were of set purpose to have covered up the traces of his investigation as if he had grudged posterity the secret of his method of inquiry while he wished to extort from them assent to his results.[60] It is possible that he used an iterative procedure to calculate these values.[61]"+
+    "In The Quadrature of the Parabola, El Grand Profeta proved that the area enclosed by a parabola and a straight line is 4/3 times the area of a corresponding inscribed triangle as shown in the figure at right. He expressed the solution to the problem as an infinite geometric series with the common ratio 1/4",
+    awpLevel:"Kill you when the round start",
+    rifleLevel:"I hit you by accident",
+    gameSence:"Your wife under my table",
+    inteligence:"Educador",
+    playerimage:"url(../images/elgrandprofeta.jpg)"
+}
+
+
--- Roster/playerDbLoader.js
+++ Roster/playerDbLoader.js
@@ -5,23 +5,11 @@ $.getScript('../DB/textDB.js', function()
     console.log("peter suce")
 
 });
+
 var rosterSelection = 0
 var rosterLoad = false;
-if(localStorage.getItem("isItRunning") == "true")
-{
-    console.log("running");
-    loadRoster1()
-    rosterLoad = false;
-    localStorage.setItem("isItRunning",rosterLoad);
-}else
-{
+var myRosterDbIndex = 0;
 
-    $(document).ready(function(){
-        $("#petercuck").click(function(){
-        setSelectionRoster(1);
-    });
-    });
-}
 function setSelectionRoster(ourSelection)
 {
     rosterSelection = ourSelection;
@@ -32,22 +20,61 @@ function setSelectionRoster(ourSelection)
 
 function onRun()
 {
-    if(localStorage.getItem("isItRunning") == true)
+    if(localStorage.getItem("isItRunning") == "true")
     {
         localStorage.setItem("rosterIndex",rosterSelection);
     }
 }
-function loadRoster1(){
-    document.getElementById("titleofPage").innerHTML=Peter.alias;
-    document.getElementById("playerName").innerHTML =Peter.name+" "+Peter.firstname;
-    document.getElementById("firstname").innerHTML = "first name: "+Peter.firstname;
-    document.getElementById("lastname").innerHTML = "last name: "+Peter.name;
-    document.getElementById("age").innerHTML = " age: "+Peter.age;
-    document.getElementById("awpLevel").innerHTML = "awp level: "+Peter.awpLevel;
-    document.getElementById("rifleLevel").innerHTML = "rifle level: "+Peter.rifleLevel;
-    document.getElementById("gamesence").innerHTML = "game sence: "+Peter.gameSence;
-    document.getElementById("inteligence").innerHTML = "inteligence: "+Peter.inteligence;
-    document.getElementById("description").innerHTML = Peter.bio;
-    document.getElementById("playerimage").style.backgroundImage = Peter.playerimage;
+
+function loadRoster(){
+    document.getElementById("titleofPage").innerHTML=myRosterDbIndex.alias;
+    document.getElementById("playerName").innerHTML =myRosterDbIndex.name+" "+myRosterDbIndex.firstname;
+    document.getElementById("firstname").innerHTML = "first name: "+myRosterDbIndex.firstname;
+    document.getElementById("lastname").innerHTML = "last name: "+myRosterDbIndex.name;
+    document.getElementById("age").innerHTML = " age: "+myRosterDbIndex.age;
+    document.getElementById("awpLevel").innerHTML = "awp level: "+myRosterDbIndex.awpLevel;
+    document.getElementById("rifleLevel").innerHTML = "rifle level: "+myRosterDbIndex.rifleLevel;
+    document.getElementById("gamesence").innerHTML = "game sence: "+myRosterDbIndex.gameSence;
+    document.getElementById("inteligence").innerHTML = "inteligence: "+myRosterDbIndex.inteligence;
+    document.getElementById("description").innerHTML = myRosterDbIndex.bio;
+    document.getElementById("playerimage").style.backgroundImage = myRosterDbIndex.playerimage;
 }
 
+
+if(localStorage.getItem("isItRunning") == "true")
+{
+    console.log("running");
+    switch(localStorage.getItem("rosterIndex"))
+    {
+        case "1": myRosterDbIndex = Peter;break;
+        case "2": myRosterDbIndex = Swifferman;break;
+        case "3": myRosterDbIndex = Glazer;break;
+        case "4": myRosterDbIndex = Elgrandprofeta;break;
+    }
+    loadRoster()
+    rosterLoad = false;
+    localStorage.setItem("isItRunning",rosterLoad);
+}else
+{
+
+    $(document).ready(function(){
+        $("#petercuck").click(function(){
+        setSelectionRoster(1);
+    });
+    });
+    $(document).ready(function(){
+        $("#swifferman").click(function(){
+        setSelectionRoster(2);
+    });
+    });
+    $(document).ready(function(){
+        $("#glazer").click(function(){
+        setSelectionRoster(3);
+    });
+    });
+    $(document).ready(function(){
+        $("#elgranprofeta").click(function(){
+        setSelectionRoster(4);
+    });
+    });
+}
